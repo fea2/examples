9121a25394d33e5d30c2af9cea2249e6853277aa
Francesco Ranaudo <ranaudo@arch.ethz.ch>
2023-06-16 07:10:09 +0200

    hanging shell problem

--- example_structures/plate.py
+++ example_structures/plate.py
@@ -1,17 +1,13 @@
 import os
-from pathlib import Path
 
 from random import choice
-from compas.geometry import Point, Vector
 from compas.datastructures import Mesh, mesh_thicken
 from compas.utilities import geometric_key_xy
 from compas_gmsh.models import MeshModel
-from compas_view2.app import App
-from compas_view2.shapes import Arrow
 
 import compas_fea2
-from compas_fea2.model import Model, DeformablePart, Node
-from compas_fea2.model import RectangularSection, ElasticIsotropic, SolidSection
+from compas_fea2.model import Model, DeformablePart
+from compas_fea2.model import ElasticIsotropic, ShellSection
 from compas_fea2.problem import Problem, StaticStep, FieldOutput
 
 from compas_fea2.units import units
@@ -28,38 +24,26 @@ TEMP = os.sep.join(HERE.split(os.sep)[:-1]+['temp'])
 # ==============================================================================
 lx = (10*units.m).to_base_units().magnitude
 ly = (10*units.m).to_base_units().magnitude
-nx = 5
-ny = 5
-mesh = Mesh.from_meshgrid(lx, nx, ly, ny)
-plate = mesh_thicken(mesh, 100)
+nx = 2
+ny = 2
+plate = Mesh.from_meshgrid(lx, nx, ly, ny)
 
 # ==============================================================================
 # Select random internal vertex for load application
 # ==============================================================================
 
-poa = choice(list(set(mesh.vertices()) - set(mesh.vertices_on_boundary())))
-poa_coordinates = mesh.vertex_coordinates(poa)
+poa = choice(list(set(plate.vertices()) - set(plate.vertices_on_boundary())))
+poa_coordinates = plate.vertex_coordinates(poa)
 
 # ==============================================================================
 # GMSH model
 # ==============================================================================
 
-model = MeshModel.from_mesh(plate, targetlength=500)
-
-# refine mesh at the point of application of the load
-model.mesh_targetlength_at_vertex(poa, 100)
-
-# refine mesh at the supports
-for vertex in mesh.vertices_where({'vertex_degree': 2}):
-    a = geometric_key_xy(mesh.vertex_coordinates(vertex))
-    for vertex in plate.vertices():
-        b = geometric_key_xy(plate.vertex_coordinates(vertex))
-        if a == b:
-            model.mesh_targetlength_at_vertex(vertex, 10)
+model = MeshModel.from_mesh(plate, targetlength=1000)
 
 model.heal()
 model.refine_mesh()
-model.generate_mesh(3)
+model.generate_mesh(2)
 # model.optimize_mesh(niter=100)
 # model.recombine_mesh()
 
@@ -67,66 +51,36 @@ model.generate_mesh(3)
 # COMPAS mesh
 # ==============================================================================
 
-solid_mesh = model.mesh_to_compas()
-lengths = [solid_mesh.edge_length(*edge) for edge in solid_mesh.edges()]
+compas_mesh = model.mesh_to_compas()
+lengths = [compas_mesh.edge_length(*edge) for edge in compas_mesh.edges()]
 print('Min length: ', min(lengths))
 print('Max length: ', max(lengths))
-
-# ==============================================================================
-# Viz
-# ==============================================================================
-
-viewer = App(width=1600, height=900)
-
-viewer.view.camera.rz = 0
-viewer.view.camera.rx = -55
-viewer.view.camera.tx = -5
-viewer.view.camera.ty = -2
-viewer.view.camera.distance = 10
-
-viewer.view.camera.target = [5000, 5000, 100]
-viewer.view.camera.position = [5000, 0, 5000]
-viewer.view.camera.near = 10
-viewer.view.camera.far = 100000
-viewer.view.camera.scale = 1000
-viewer.view.grid.cell_size = 1000
-
-viewer.add(solid_mesh)
-
-poa = Point(* plate.vertex_coordinates(poa))
-start = poa + Vector(0, 0, 1000)
-vector = Vector(0, 0, -1000)
-load = Arrow(start, vector, body_width=0.02)
-
-viewer.add(poa, pointsize=5)
-viewer.add(load, facecolor=(1, 0, 0))
-
-viewer.run()
 
 # ==============================================================================
 # COMPAS_FEA2
 # ==============================================================================
 
 # Initialize model
-mdl = Model(name='mesh_refine')
+mdl = Model(name='plate')
 # Define some properties
 mat = ElasticIsotropic(E=(210*units.GPa).to_base_units().magnitude, 
                        v=0.2, 
                        density=(7800*units("kg/m**3")).to_base_units().magnitude)
-sec = SolidSection(material=mat)
+sec = ShellSection(material=mat, t=100)
 
 # Convert the gmsh model in a compas_fea2 Part
-prt = DeformablePart.from_gmsh(gmshModel=model, section=sec)
-prt.ndf=3 # this is needed for the opensees FourNodeTetrahedron model
-prt._discretized_boundary_mesh = solid_mesh
+prt = DeformablePart.from_gmsh(gmshModel=model, section=sec, implementation='shelldkgt')
+# prt.ndf=6 # this is needed for the opensees FourNodeTetrahedron model
+prt._discretized_boundary_mesh = compas_mesh
 mdl.add_part(prt)
 
 # Set boundary conditions in the corners
-for vertex in mesh.vertices_where({'vertex_degree': 2}):
-    location = mesh.vertex_coordinates(vertex)
+for vertex in plate.vertices_where({'vertex_degree': 2}):
+    location = plate.vertex_coordinates(vertex)
     mdl.add_fix_bc(nodes=prt.find_nodes_by_location(location, distance=150))
 
 # mdl.summary()
+# mdl.show()
 
 # Initialize a step
 stp = StaticStep()
@@ -148,7 +102,9 @@ prb.add_step(stp)
 mdl.add_problem(problem=prb)
 
 # Analyze and extracte results to SQLite database
+# mdl.analyse(problems=[prb], path=Path(TEMP).joinpath(prb.name), verbose=True)
 mdl.analyse_and_extract(problems=[prb], path=TEMP, verbose=True)
 
 # Show Results
-prb.show_displacements(draw_loads=100)
+# prb.show_displacements(show_deformed=100)
+# prb.show_deformed()
